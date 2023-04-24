41e7c83d8f48a3f4ccc931e8ccb0e4eac07bdf1a
Dom Wong <dwong@palo-it.com>
2023-04-24 13:48:51 +0800

    convert lengthy code to module

--- infra/main.tf
+++ infra/main.tf
@@ -29,41 +29,6 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
   acl    = "private"
 }
 
-data "archive_file" "lambda_functions" {
-  type        = "zip"
-  source_dir  = "../backend/src"
-  output_path = "../backend.zip"
-}
-
-resource "aws_s3_object" "lambda_functions" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-
-  key    = "backend.zip"
-  source = data.archive_file.lambda_functions.output_path
-
-  etag = filemd5(data.archive_file.lambda_functions.output_path)
-}
-
-resource "aws_lambda_function" "hello_world" {
-  function_name = "Hello_${var.stage}"
-
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_functions.key
-
-  runtime = "nodejs16.x"
-  handler = "hello.handler"
-
-  source_code_hash = data.archive_file.lambda_functions.output_base64sha256
-
-  role = aws_iam_role.lambda_exec.arn
-}
-
-resource "aws_cloudwatch_log_group" "hello_world" {
-  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
-
-  retention_in_days = 30
-}
-
 resource "aws_iam_role" "lambda_exec" {
   name = "serverless_lambda_${var.stage}"
 
@@ -81,67 +46,44 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+# Define the API Gateway REST API
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
 
-resource "aws_apigatewayv2_api" "lambda" {
-  name          = "lambda_function_${var.stage}"
-  protocol_type = "HTTP"
-}
+  create_api_domain_name = false
+  name                   = "hello-world-v2-${var.stage}"
 
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
+  integrations = {
+    "GET /hello" = {
+      lambda_arn = module.lambda_function.lambda_function_arn
+    }
+  }
 
-  name        = var.stage
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
+  tags = {
+    Name = "${var.stage}"
   }
 }
 
-resource "aws_apigatewayv2_integration" "hello_world" {
-  api_id = aws_apigatewayv2_api.lambda.id
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
 
-  integration_uri    = aws_lambda_function.hello_world.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
+  function_name = "hello-world-${var.stage}"
+  description   = "Lambda test module"
+  handler       = "hello.handler"
+  runtime       = "nodejs16.x"
+  publish       = true
 
-resource "aws_apigatewayv2_route" "hello_world" {
-  api_id = aws_apigatewayv2_api.lambda.id
+  source_path = "../backend/src/"
+  store_on_s3 = true
+  s3_bucket   = aws_s3_bucket.lambda_bucket.id
 
-  route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
-}
-
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}_${var.stage}"
-
-  retention_in_days = 30
-}
-
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service    = "apigateway"
+      source_arn = "${module.api_gateway.apigatewayv2_api_execution_arn}/*/*"
+    }
+  }
+  environment_variables = {
+    "Serverless" = "Terraform"
+  }
 }
