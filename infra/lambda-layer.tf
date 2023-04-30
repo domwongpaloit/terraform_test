4b23a70c67f1382407e4be5a48ce75c5157c2da6
Dom Wong <dwong@palo-it.com>
2023-05-01 02:45:37 +0800

    combine

--- infra/lambda-layer.tf
+++ infra/lambda-layer.tf
@@ -8,7 +8,7 @@ module "lambda_layer_s3" {
   description         = "My amazing lambda layer (deployed from S3)"
   compatible_runtimes = ["nodejs16.x"]
 
-  source_path = "../${path.module}/backend/layer"
+  source_path = "../backend/layer"
 
   store_on_s3 = true
   s3_bucket   = aws_s3_bucket.lambda_bucket.id
