dcde1ef8569e195d4bf1ecfc128eadeaa0534135
Dom Wong <dwong@palo-it.com>
2023-05-01 02:36:30 +0800

    dynamic stage layer

--- infra/lambda-layer.tf
+++ infra/lambda-layer.tf
@@ -4,12 +4,12 @@ module "lambda_layer_s3" {
 
   create_layer = true
 
-  layer_name          = "my-layer-s3"
+  layer_name          = "my-layer-s3-${var.stage}"
   description         = "My amazing lambda layer (deployed from S3)"
   compatible_runtimes = ["nodejs16.x"]
 
   source_path = "../${path.module}/backend/layer"
 
-  #   store_on_s3 = true
-  #   s3_bucket   = aws_s3_bucket.lambda_bucket.id
+  store_on_s3 = true
+  s3_bucket   = aws_s3_bucket.lambda_bucket.id
 }
