9c926b31f9e9a90e14098edbdee2c037bb163e7b
Dom Wong <dwong@palo-it.com>
2023-05-01 14:51:54 +0800

    revert

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -13,75 +13,24 @@ locals {
   ]
 }
 
-# module "lambda_functions" {
-#   source = "terraform-aws-modules/lambda/aws"
-
-#   for_each = { for fn in local.lambda_functions : fn.function_name => fn }
-
-#   function_name = "${each.value.function_name}-${var.stage}"
-#   description   = "Lambda test module"
-#   handler       = each.value.handler
-#   runtime       = each.value.runtime
-#   publish       = true
-#   source_path   = "../backend/src/${each.value.function_name}.js"
-#   store_on_s3   = true
-#   s3_bucket     = aws_s3_bucket.lambda_bucket.id
-
-#   layers = [
-#     module.lambda_layer_s3.lambda_layer_arn,
-#   ]
-
-#   allowed_triggers = {
-#     AllowExecutionFromAPIGateway = {
-#       service    = "apigateway"
-#       source_arn = "${module.api_gateway.apigatewayv2_api_execution_arn}/*/*"
-#     }
-#   }
-#   environment_variables = {
-#     "Serverless" = "Terraform"
-#   }
-# }
-
-module "lambda_hello" {
+module "lambda_functions" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "${local.lambda_functions[0].function_name}-${var.stage}"
+  for_each = { for fn in local.lambda_functions : fn.function_name => fn }
+
+  function_name = "${each.value.function_name}-${var.stage}"
   description   = "Lambda test module"
-  handler       = local.lambda_functions[0].handler
-  runtime       = local.lambda_functions[0].runtime
+  handler       = each.value.handler
+  runtime       = each.value.runtime
   publish       = true
-  source_path   = "../backend/src/${local.lambda_functions[0].function_name}.js"
+  source_path   = "../backend/src/${each.value.function_name}.js"
   store_on_s3   = true
   s3_bucket     = aws_s3_bucket.lambda_bucket.id
 
   layers = [
     module.lambda_layer_s3.lambda_layer_arn,
   ]
-  allowed_triggers = {
-    AllowExecutionFromAPIGateway = {
-      service    = "apigateway"
-      source_arn = "${module.api_gateway.apigatewayv2_api_execution_arn}/*/*"
-    }
-  }
-  environment_variables = {
-    "Serverless" = "Terraform"
-  }
-}
 
-module "lambda_getsomething" {
-  source = "terraform-aws-modules/lambda/aws"
-
-  function_name = "${local.lambda_functions[1].function_name}-${var.stage}"
-  description   = "Lambda test module"
-  handler       = local.lambda_functions[1].handler
-  runtime       = local.lambda_functions[1].runtime
-  publish       = true
-  source_path   = "../backend/src/${local.lambda_functions[1].function_name}.js"
-  store_on_s3   = true
-  s3_bucket     = aws_s3_bucket.lambda_bucket.id
-  layers = [
-    module.lambda_layer_s3.lambda_layer_arn,
-  ]
   allowed_triggers = {
     AllowExecutionFromAPIGateway = {
       service    = "apigateway"
