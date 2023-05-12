fca1a8de17c6b4c8aa875248a4ff4978dcaf234f
Dom Wong <dwong@palo-it.com>
2023-05-12 14:20:05 +0800

    test one more blue green

--- backend/src/hello.js
+++ backend/src/hello.js
@@ -8,9 +8,9 @@
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
   console.log(process.env.NODE_PATH);
-  let responseMessage = 'V70Hello, World!';
+  let responseMessage = 'V71Hello, World!';
   if (event.queryStringParameters && event.queryStringParameters['Name']) {
-    responseMessage = 'V70Hello, ' + event.queryStringParameters['Name'] + '!';
+    responseMessage = 'V71Hello, ' + event.queryStringParameters['Name'] + '!';
   }
   return {
     statusCode: 200,
