6e68965635f9b165514f05faa230a5c75d822997
Dom Wong <dwong@palo-it.com>
2023-05-18 13:12:15 +0800

    test deploy

--- backend/__test__/hello.test.js
+++ backend/__test__/hello.test.js
@@ -5,7 +5,7 @@ describe('handler', () => {
         const event = {};
         const response = await handler(event);
         expect(response.statusCode).toEqual(200);
-        expect(response.body).toEqual(JSON.stringify({ message: 'V71Hello, World!' }));
+        expect(response.body).toEqual(JSON.stringify({ message: 'V72Hello, World!' }));
     });
 
     it('returns a response with "Hello, <name>!" when called with a "Name" query parameter', async () => {
@@ -17,6 +17,6 @@ describe('handler', () => {
         };
         const response = await handler(event);
         expect(response.statusCode).toEqual(200);
-        expect(response.body).toEqual(JSON.stringify({ message: `V71Hello, ${name}!` }));
+        expect(response.body).toEqual(JSON.stringify({ message: `V72Hello, ${name}!` }));
     });
 });
